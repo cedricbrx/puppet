6c60a556174fe0b658311320af5c5be206daf629
cedricbrx <cedricbr@hotmail.com>
2017-09-12 16:18:40 +0200

    Update site-ubuntu.pp

--- site-ubuntu.pp
+++ site-ubuntu.pp
@@ -87,10 +87,6 @@ class multimedia {
 	package {"rhythmbox":
 		ensure => purged,
 	}
-	package {"flashplugin-installer":
-		ensure => purged,
-		require => Package["ubuntu-restricted-extras"],
-	}
 	exec {'configure-libdvd-pkg':
 		command => '/usr/bin/debconf-set-selections <<< "libdvd-pkg libdvd-pkg/post-invoke_hook-install boolean true\nlibdvd-pkg libdvd-pkg/build boolean true"',
 		unless  => '/usr/bin/debconf-get-selections | /bin/grep "libdvd-pkg/post-invoke_hook-install"',
